dbd9d9c3e4e4d50529e87712087fd6918c30d43b
SBreso <sbresograncha@gmail.com>
2019-01-11 10:13:37 +0100

    init

---
+++ SignalR.Contracts/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace SignalR.Contracts
+{
+    public class ChatHub: Microsoft.AspNetCore.SignalR.Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            string[] args = new string[] { user, message };
+            await Clients.All.SendCoreAsync("ReceiveMessage", args);
+        }
+    }
+}
---
+++ SignalRClient/wwwroot/js/chat.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+//var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:44386/chatHub").build();
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+//El código anterior:
+//Crea e inicia una conexión.
+//Agrega al botón de envío un controlador que envía mensajes al concentrador.
+//Agrega al objeto de conexión un controlador que recibe mensajes desde el concentrador y los agrega a la lista.
